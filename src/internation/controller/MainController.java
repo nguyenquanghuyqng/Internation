8784982717210189b9a4a132fe404d9b2ad49f1b
Nguyễn Quang Huy <nguyenquanghuy605@gmail.com>
2018-04-24 09:58:34 +0700

    Tableview
    Sử dụng table view

--- src/internation/controller/MainController.java
+++ src/internation/controller/MainController.java
@@ -13,6 +13,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
 import javafx.scene.control.Slider;
+import javafx.scene.control.TableView;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
@@ -25,6 +26,8 @@ public class MainController implements Initializable {
 	private MediaPlayer mp;
 	private Media me;
 	private Slider slider;
+	@FXML
+	private TableView tableview;
 //	final VBox vbox = new VBox();
 	
 	public static final ObservableList subtime = 
